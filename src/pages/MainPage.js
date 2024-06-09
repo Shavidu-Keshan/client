19f1c4f502d6bbf332a697c9b169bcff24fffabc
Shavidu Keshan <jkskeshan@sat.appsc.sab.ac.lk>
2024-06-09 09:59:45 +0530

    i add date tab

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 export default function MainPage() {
   return <div>
-    <h1 className="text-5xl"> Convert Your Currencies Today</h1>
-    <p>CurrencyConvert is a versatile currency exchange app designed for travelers, shoppers, and professionals. It offers real-time exchange rates, historical data, and offline mode, providing accurate and hassle-free currency conversions. With a user-friendly interface and multi-currency support, it helps users make informed financial decisions globally.</p>
+    <h1 className="lg:mx-32 text-5xl font-bold text-green-500"> Convert Your Currencies Today</h1>
+    <p className=' lg:mx-32 opacity-40 py-6'>CurrencyConvert is a versatile currency exchange app designed for travelers, shoppers, and professionals. It offers real-time exchange rates, historical data, and offline mode, providing accurate and hassle-free currency conversions. With a user-friendly interface and multi-currency support, it helps users make informed financial decisions globally.</p>
+    
+    <div>
+       <section>
+        <form>
+            <div>
+                <label htmlFor="">date</label>
+                <input type='date'/>
+            </div>
+        </form>
+
+        </section> 
+    </div>
   </div>
 }
