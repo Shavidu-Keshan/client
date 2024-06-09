4c47f032f0142b36a52e459eb6d52cb6d13b0363
Shavidu Keshan <jkskeshan@sat.appsc.sab.ac.lk>
2024-06-09 09:13:54 +0530

    I install tailwindcss and give the topic and introuduction for our app

---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function MainPage() {
+  return <div>
+    <h1 className="text-5xl"> Convert Your Currencies Today</h1>
+    <p>CurrencyConvert is a versatile currency exchange app designed for travelers, shoppers, and professionals. It offers real-time exchange rates, historical data, and offline mode, providing accurate and hassle-free currency conversions. With a user-friendly interface and multi-currency support, it helps users make informed financial decisions globally.</p>
+  </div>
+}
